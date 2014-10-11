2e87ba47add12a1b11fc340b67497be98799e059
Ali Haris <aharis11@gmail.com>
2014-10-11 16:54:11 +0530

    Example implementation of OOP

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
 
 /**
- * Maldives Weather Information
- * @author Ali Haris (http://github.com/aliharis)
+ * Maldives Weather Information Wrapper
+ * Retrives weather information from Dept. of Met
+ * @author Ali Haris (www.github.com/aliharis)
  */
 
 header("Content-type: application/json;charset=utf-8");
 
 require 'simple_html_dom.php';
-require 'functions.php';
+require 'MvWeather.php';
+
+$dom = new simple_html_dom();
+$Weather = new MvWeather($dom);
 
 /** Handle the request */
-$locations = array('Male', 'Kadhdhoo', 'Kaadedhdhoo', 'Hanimaadhoo', 'Gan');
+$locations = $Weather->getLocations();
 $request = (!empty($_GET['location'])) ? $_GET['location'] : '';
 
 if (in_array($request, $locations)):
-	echo json_encode($weatherDetails($request));
+	echo json_encode($Weather->getDetails($request));
 
 else:
-	echo json_encode($weatherSummary($locations));	 
+	echo json_encode($Weather->getSummary());	 
 endif;
-
-
